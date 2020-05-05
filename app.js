468b4c2d11428ab9c3dbb29dd8404ed65143ce04
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2020-05-05 11:55:16 +0900

    update electron

--- app.js
+++ app.js
@@ -1,41 +1,172 @@
+//モジュールの読み込み
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const join = require('path').join;
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//passportの設定
+var passport = require('passport');
+var strategy = require('passport-azure-ad').OIDCStrategy;
+passport.serializeUser(function (user, done)
+{
+  done(null, user);
+});
+passport.deserializeUser(function (user, done)
+{
+  done(null, user);
+});
 
+//認証用データ
+var auth = "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0/.well-known/openid-configuration";
+var redirecturi = "http://localhost:3000/auth/callback";
+var scope = [
+  'profile',
+  'offline_access',
+  'user.read',
+  'Files.ReadWrite'
+]
+var clientID = "26f733c3-0c7c-44e8-aa39-29d2cb805afc";
+var clientsecret = "od4OU:Ee_KODvU[DPZRu6nE4GkjA.y56";
+
+//Azure AD認証用のオプション
+var options = {
+  identityMetadata: auth,
+  clientID: clientID,
+  responseType: "code",
+  responseMode: "form_post",
+  redirectUrl: redirecturi,
+  allowHttpForRedirectUrl: true,
+  clientSecret: clientsecret,
+  validateIssuer: false,
+  scope: scope,
+  passReqToCallback: false
+};
+
+//認証フロー
+passport.use(new strategy(options,
+  (iss, sub, profile, access_token, refresh_token, done) =>
+  {
+    try
+    {
+      if (profile.oid)
+      {
+        //データを組み立て
+        const user = {
+          iss,
+          sub,
+          profile,
+          access_token,
+          refresh_token
+        };
+
+        //Access Tokenをテキストに書き出す
+        fs.writeFileSync("user.txt", user.access_token);
+
+        //ユーザデータを返す
+        return done(null, user);
+      }
+
+      //取得データを返す
+      return done(null, false);
+
+    } catch (err)
+    {
+      //エラートラップ
+      return done(null, err);
+    }
+  }
+));
+
+//expressの設定
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//セッション
+app.use(session({
+  secret: 'graphman super',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: false
+  }
+}));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//ビューテンプレート
+app.set('views', join(__dirname, '/views'));　//index.htmlを読み込ませる
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//passportの初期化
+app.use(passport.initialize());
+app.use(passport.session());
+
+//その他の設定
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//ルーティング
+//★初期ページ
+app.get('/', (req, res) =>
+{
+  //ejsの力でindex.html側へデータを反映する
+  res.render('index.html', { user: req.user });
+});
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+//サインインクリック時
+app.get('/auth/signin',
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/' })
+  , (req, res) =>
+  {
+    res.redirect('/');
+  }
+);
+
+//サインイン後のコールバックを受け取った時
+app.post('/auth/callback',
+  passport.authenticate('azuread-openidconnect', { failureRedirect: '/' })
+  , (req, res) =>
+  {
+    res.redirect('/');
+  }
+);
+
+//サインアウトをクリックした時
+app.get('/auth/signout',
+  (req, res) =>
+  {
+    req.session.destroy((err) =>
+    {
+      if (err)
+      {
+        res.send(err)
+        return
+      }
+      res.redirect('/')
+    })
+  }
+);
+
+//404検知
+app.use(function (req, res, next)
+{
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//エラーハンドラー（リリース直前までコメントアウト推奨）
+//app.use(function(err, req, res, next) {
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//エラーページを表示
+//res.status(err.status || 500);
+//res.render('error');
+//});
 
+//サーバ開始
 module.exports = app;
